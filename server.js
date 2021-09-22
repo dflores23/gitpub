fdd7f450235db0647a236629d39577b438522ef0
David Flores <davidafloresvel@gmail.com>
2021-09-22 16:19:34 -0600

    ....

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const port = 3000;
 const drinks = require("./models/drinks");
 const food = require("./models/food");
 
-// function capitalize(drinks) {
-//     const newDrink = {...drinks}
-//     newDrink.name = drinks.name.charArt(0).toUpperCase() + drinks.slice(1)
-//     return newDrink
-// }
+function capitalize(drinks) {
+    const newDrink = {...drinks, ...food}
+    newDrink = drinks.name.charArt(1).toUpperCase() + drinks.slice(1);
+    return newDrink
+}
 
 
 app.get(`/`, (req, res)=>{
@@ -16,18 +16,18 @@ app.get(`/`, (req, res)=>{
 });
 
 app.get(`/drinks/`, (req, res)=> {
-    res.render("drinks_index.ejs", {allDrinks : drinks});
+    res.render("drinks_index.ejs", {allDrinks : drinks,});
 });
 
 app.get(`/food/`, (req, res)=> {
     res.render("food.ejs", {allFood : food});
 });
 
-// app.get("/drinks/:indexOfDrinksArray", (req, res) => {
-//     res.render("drinks_show.ejs", { drink: drinks[req.params.indexOfDrinksArray], title: "First - Show Page" });
-//   });
+app.get("/drinks/:id", (req, res) => {
+    res.send(req.params.id);
+  });
 
 app.listen(3000, ()=> {
     console.log(`listening to port ${port}`)
 });
-// console.log(drink)
+console.log(food, drinks)
